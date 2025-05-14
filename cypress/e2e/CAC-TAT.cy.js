f0a665b393c8cae124971774b46373f6689456a3
BrendoLopez <brendoslopes@gmail.com>
2025-05-14 10:04:43 -0300

    Aprendendo comandos customizados com Cypress e manipulando objetos

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -1,64 +1,138 @@
 /// <reference types="cypress" />
-describe('Central de Atendimento ao Cliente TAT', () => {
-  beforeEach(() =>{
-    cy.visit('./src/index.html')
-  })
+describe("Central de Atendimento ao Cliente TAT", () => {
+  beforeEach(() => {
+    cy.visit("./src/index.html");
+  });
 
-  it('verifica o título da aplicação', () => {
-    cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
-  })
+  it("verifica o título da aplicação", () => {
+    cy.title().should("be.equal", "Central de Atendimento ao Cliente TAT");
+  });
 
-  it('preenche os campos obrigatórios e envia o formulário', () =>{
-    cy.get('#firstName')
-    .should('be.visible')
-    .type('Brendo')
-    .should('have.value', 'Brendo')
+  it("preenche os campos obrigatórios e envia o formulário", () => {
+    cy.get("#firstName")
+      .should("be.visible")
+      .type("Brendo")
+      .should("have.value", "Brendo");
 
-    cy.get('#lastName')
-    .should('be.visible')
-    .type('Lopes')
-    .should('have.value', 'Lopes')
+    cy.get("#lastName")
+      .should("be.visible")
+      .type("Lopes")
+      .should("have.value", "Lopes");
 
-    cy.get('#email')
-    .should('be.visible')
-    .type('brendoslopes@gmail.com')
-    .should('have.value', 'brendoslopes@gmail.com')
+    cy.get("#email")
+      .should("be.visible")
+      .type("brendoslopes@gmail.com")
+      .should("have.value", "brendoslopes@gmail.com");
 
-    cy.get('#open-text-area')
-    .should('be.visible')
-    .type('Estou em dúvida no que escrever neste campo', { delay: 0})
-    .should('have.value', 'Estou em dúvida no que escrever neste campo')
-    
-    cy.get('button[type="submit"]').click()
+    cy.get("#open-text-area")
+      .should("be.visible")
+      .type("Estou em dúvida no que escrever neste campo", { delay: 0 })
+      .should("have.value", "Estou em dúvida no que escrever neste campo");
 
-    cy.get('.success')
-    .should('be.visible')
-  })
+    cy.get('button[type="submit"]').click();
 
-  it('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', () =>{
-    cy.get('#firstName')
-    .should('be.visible')
-    .type('Brendo')
-    .should('have.value', 'Brendo')
+    cy.get(".success").should("be.visible");
+  });
 
-    cy.get('#lastName')
-    .should('be.visible')
-    .type('Lopes')
-    .should('have.value', 'Lopes')
+  it("exibe mensagem de erro ao submeter o formulário com um email com formatação inválida", () => {
+    cy.get("#firstName")
+      .should("be.visible")
+      .type("Brendo")
+      .should("have.value", "Brendo");
 
-    cy.get('#email')
-    .should('be.visible')
-    .type('errandoEmail')
-    .should('have.value', 'errandoEmail')
+    cy.get("#lastName")
+      .should("be.visible")
+      .type("Lopes")
+      .should("have.value", "Lopes");
 
-    cy.get('#open-text-area')
-    .should('be.visible')
-    .type('Estou em dúvida no que escrever neste campo', { delay: 0})
-    .should('have.value', 'Estou em dúvida no que escrever neste campo')
+    cy.get("#email")
+      .should("be.visible")
+      .type("errandoEmail")
+      .should("have.value", "errandoEmail");
 
-    cy.get('button[type="submit"]').click()
+    cy.get("#open-text-area")
+      .should("be.visible")
+      .type("Estou em dúvida no que escrever neste campo", { delay: 0 })
+      .should("have.value", "Estou em dúvida no que escrever neste campo");
 
-    cy.get('.error')
-    .should('be.visible')
-  })
-})
+    cy.get('button[type="submit"]').click();
+
+    cy.get(".error").should("be.visible");
+  });
+
+  it("não permite valores não-numéricos no campo de telefone", () => {
+    cy.get("#phone").should("be.visible").type("e+").should("have.value", "");
+  });
+
+  it("exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário", () => {
+    cy.get("#firstName")
+      .should("be.visible")
+      .type("Brendo")
+      .should("have.value", "Brendo");
+
+    cy.get("#lastName")
+      .should("be.visible")
+      .type("Lopes")
+      .should("have.value", "Lopes");
+
+    cy.get("#email")
+      .should("be.visible")
+      .type("brendoslopes@gmail.com")
+      .should("have.value", "brendoslopes@gmail.com");
+
+    cy.get("#open-text-area")
+      .should("be.visible")
+      .type("Estou em dúvida no que escrever neste campo", { delay: 0 })
+      .should("have.value", "Estou em dúvida no que escrever neste campo");
+
+    cy.get("#phone-checkbox").click();
+
+    cy.get('button[type="submit"]').click();
+
+    cy.get(".error").should("be.visible");
+  });
+
+  it("preenche e limpa os campos nome, sobrenome, email e telefone", () => {
+    cy.get("#firstName")
+      .type("Brendo")
+      .should("have.value", "Brendo")
+      .clear()
+      .should("have.value", "");
+
+    cy.get("#lastName")
+      .type("Lopes")
+      .should("have.value", "Lopes")
+      .clear()
+      .should("have.value", "");
+
+    cy.get("#email")
+      .type("brendoslopes@gmail.com")
+      .should("have.value", "brendoslopes@gmail.com")
+      .clear()
+      .should("have.value", "");
+
+    cy.get("#phone")
+      .type("27996087238")
+      .should("have.value", "27996087238")
+      .clear()
+      .should("have.value", "");
+  });
+
+  it("exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios", () => {
+    cy.get(".button").click();
+
+    cy.get(".error").should("be.visible");
+  });
+
+  it.only("envia o formuário com sucesso usando um comando customizado", () => {
+    const data = {
+      firstName: 'Brendo',
+      lastName: 'Lopes',
+      email: 'brendoslopes@gmail.com',
+      text: 'Teste'
+    }
+    cy.fillMandatoryFieldsAndSubmit(data);
+
+    cy.get(".success").should("be.visible");
+  });
+});
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,25 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+/// <reference types="cypress" />
+
+Cypress.Commands.add('fillMandatoryFieldsAndSubmit', data => {
+    cy.get("#firstName")
+      .should("be.visible")
+      .type(data.firstName)
+      .should("have.value", "Brendo");
+
+    cy.get("#lastName")
+      .should("be.visible")
+      .type(data.lastName)
+      .should("have.value", "Lopes");
+
+    cy.get("#email")
+      .should("be.visible")
+      .type(data.email)
+      .should("have.value", "brendoslopes@gmail.com");
+
+    cy.get("#open-text-area")
+      .should("be.visible")
+      .type(data.text, { delay: 0 })
+      .should("have.value", "Teste");
+
+    cy.get('button[type="submit"]').click();
+})
